b65d332b398afbbe7bf2c1ccf45b02e1dd11c43f
sh32321 <sh32321@gmail.com>
2022-02-22 19:38:49 -0800

    unfinish course selection

--- src/container/CourseSelectionContainer.jsx
+++ src/container/CourseSelectionContainer.jsx
@@ -1,7 +1,19 @@
-import React from "react";
-
+import React, { useState, useRef } from "react";
+import Carousel from "../components/Carousel";
+import { pythonData } from "../course-selection-data/data";
+import { js } from "../course-selection-data/data";
 const CourseSelectionContainer = () => {
-  return <div>CourseSelectionContainer</div>;
+  const [triggerElement, setTriggerElement] = useState("Python");
+
+  return (
+    <>
+      <button onClick={() => setTriggerElement("Python")}>Python</button>
+      <button onClick={() => setTriggerElement("JS")}>JS</button>
+      {triggerElement === "Python" && <Carousel data={pythonData} />}
+      {triggerElement === "JS" && <Carousel data={js} />}
+      {/* <Carousel /> */}
+    </>
+  );
 };
 
 export default CourseSelectionContainer;
