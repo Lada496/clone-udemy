dca19b292e84bb0a50c9df45d2e4ca0f845121c9
Lada496 <67321065+Lada496@users.noreply.github.com>
2022-03-01 16:11:15 -0800

    fixed RightTooltip Button style

--- src/components/Navigation/RightTooltip/RightTooltip.styles.js
+++ src/components/Navigation/RightTooltip/RightTooltip.styles.js
@@ -1,4 +1,12 @@
 import styled from "styled-components";
+
+export const CardContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 2rem;
+`;
 export const TextContainer = styled.p`
   font-weight: 600;
   font-size: 2.2rem;
